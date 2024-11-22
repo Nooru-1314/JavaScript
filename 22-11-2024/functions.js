be3a7daedd9d234602a9fbc180ab8b79bb505da4
S Noorunnisa <shaiknoorunnisa45@gmail.com>
2024-11-22 15:42:08 +0530

    simple Programs on functions

---
+++ 22-11-2024/functions.js
@@ -0,0 +1,25 @@
+function hello(){
+    return "Hello world!";
+    }
+// Multiple functions
+function a(){
+    return "Hello a!";
+    } 
+    function b(){
+    return "Hello b!";
+    }
+
+// function call
+
+function greet(){
+    return "Haydo!";
+    }
+    let salutation = greet();
+//functions with parameters
+
+function echo(str){
+    return str;
+    }
+    console.log(echo("Greta"));
+    console.log(echo("CO2"));
+    
